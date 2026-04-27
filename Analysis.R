9389ad1ecba9662eae5ac5ec6c6d6a0099f44478
ViktorGaardman <viktorg97@live.se>
2026-04-27 21:39:46 +0200

    RDA, GLM and basic graphs

--- Analysis.R
+++ Analysis.R
@@ -1,247 +1,318 @@
 library(tidyverse)
 library(vegan)
+library(ggrepel)
+library(car)
+library(MASS)
+library(DHARMa)
+library(ggeffects)
 
-df <- read.csv("combined_dataset_bearfeces.csv")
+rm(list=ls())
 
-df <- df %>%
-  mutate(across(c("Sample", "Bear", "Species"), as.factor))
+options(contrasts = c("contr.sum", "contr.poly"))
 
-#Summary plots
-#divide Date_2 into one column for DOY
-#and another for year
+set.seed(123)
 
-df$date_parsed <- as.Date(df$Date_2, format = "%d.%m.%y")
-df$year <- as.numeric(format(df$date_parsed, "%Y"))
-df$doy  <- as.numeric(format(df$date_parsed, "%j"))
-df$North <- as.numeric(df$North)
-
-df$date_parsed_start <- as.Date(df$Date_1, format = "%d.%m.%y")
-df$doy_start  <- as.numeric(format(df$date_parsed_start, "%j"))
-
-df$dung_age <- df$doy - df$doy_start
-
-df$dung_age[is.na(df$dung_age)] <- df$days_old[is.na(df$dung_age)]
+df <- read.csv("clean_df.csv", sep = ";")
 
 df <- df %>%
-  mutate(
-    herbivory = rowSums(across(c(Diet_Oats, Diet_Blueberries, Diet_Apple,
-                                 Diet_Green, Diet_Lingonberries, Diet_Cloudberries,
-                                 Diet_Sorbus)), na.rm = TRUE),
-    carnivory = rowSums(across(c(Diet_Ants, Diet_Meat, Diet_Pork, Diet_Eggs,
-                                 Diet_Moose, Diet_Bees)), na.rm = TRUE)
-  )
-
-df$HerbivoreDiet <- ifelse(df$herbivory > 50, 1, 0)
-
-df_bears <- df %>%
-  filter(!is.na(Bear), Bear != "")
-
-df_bears$Bear[df_bears$Bear == "Topi/K3"] <- "Topi"
-
-df_bears$HerbivoreDiet <- as.factor(df_bears$HerbivoreDiet) 
-
-data_summary <- ggplot(df_bears, aes(x = doy, y = North, color = Bear)) +
-  geom_point(aes(shape = HerbivoreDiet), size = 3) +
-  labs(
-    x = "Day of Year",
-    y = "Latitude"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title.y = element_text(size = 14),
-    axis.title.x = element_text(size = 14),
-    legend.text = element_text(size = 12),     
-    legend.title = element_text(size = 14),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.line.x = element_line(color = "black"),
-    axis.line = element_line(color = "black"))
-
-ggsave(data_summary, filename = "bear_datasum.png",
-       width = 6.5, height = 5.26 , dpi = 300)
-
-data_summary2 <- ggplot(df_bears, aes(x = days_old, y = North, color = Bear)) +
-  geom_point(aes(shape = HerbivoreDiet), size = 3) +
-  labs(
-    x = "Age of dung",
-    y = "Latitude"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title.y = element_text(size = 14),
-    axis.title.x = element_text(size = 14),
-    legend.text = element_text(size = 12),     
-    legend.title = element_text(size = 14),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.line.x = element_line(color = "black"),
-    axis.line = element_line(color = "black"))
-
-ggsave(data_summary2, filename = "bear_datasum2.png",
-       width = 6.5, height = 5.26 , dpi = 300)
-
-df_summary <- df %>%
-  dplyr::group_by(doy, dung_age, North) %>%
-  dplyr::summarise(n = dplyr::n_distinct(Sample))
-
-data_summary3 <- ggplot(df_summary, aes(x = dung_age, y = North)) +
-  geom_point(aes(size = n)) +
-  scale_size_continuous(
-    range = c(2, 12),
-    breaks = c(1, 2, 3)
-  ) +
-  labs(
-    x = "Age of dung",
-    y = "Latitude",
-    size = "Number of samples"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title.y = element_text(size = 14),
-    axis.title.x = element_text(size = 14),
-    legend.text = element_text(size = 12),     
-    legend.title = element_text(size = 14),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.line.x = element_line(color = "black"),
-    axis.line = element_line(color = "black")) +
-  scale_y_continuous(limits = c(610000, 640000))
-
-data_summary3
-
-ggsave(data_summary3, filename = "bear_datasum3.png",
-       width = 6.5, height = 5.26 , dpi = 300)
-
-
-#Calculate species richness and abundance per sample
-df_richness <- df %>%
-  group_by(Sample, dung_age, North, doy) %>%
-  summarize(
-  sp_richness = n_distinct(Species),
-  abundance = sum(Count)
-    )
-
-#Quickplot
-
-ggplot(df_richness, aes(x = doy, y = sp_richness, color = dung_age)) +
-  geom_point() +
-  labs(
-    x = "Day of Year",
-    y = "Species richness",
-    color = "Age of dung"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title.y = element_text(size = 14),
-    axis.title.x = element_text(size = 14),
-    legend.text = element_text(size = 12),     
-    legend.title = element_text(size = 14),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.line.x = element_line(color = "black"),
-    axis.line = element_line(color = "black"))
-
-ggplot(df_richness, aes(x = North, y = sp_richness, color = dung_age)) +
-  geom_point() +
-  labs(
-    x = "Latitude",
-    y = "Species richness",
-    color = "Age of dung"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title.y = element_text(size = 14),
-    axis.title.x = element_text(size = 14),
-    legend.text = element_text(size = 12),     
-    legend.title = element_text(size = 14),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.line.x = element_line(color = "black"),
-    axis.line = element_line(color = "black"))
-
-### Delete non-dung species
-
-df <- df %>% 
-  mutate(Species = str_trim(Species)) %>% 
-  filter(!Species %in% c(
-    "Apion_simile",
-    "Cyphon punctipennis",
-    "Deporaus betulae",
-    "Epuraea sp.",
-    "Glischrochilus quadripunctatus",
-    "EI KUORIAISIA",
-    "Malthodes fuscus",
-    "Phloeonomus sjobergi",
-    "Strophosoma capitatum",
-    "Zyras humeralis",
-    "Nicrophorus investigator",
-    "Nicrophorus vespilloides",
-    "Pterostichus diligens"
-  ))
-  
-#Split doy into spring, summer, autumn ish.
-#Split dung age into fresh (1-2 days), intermediate (3-5 days), old (5-8),
-#very old 8+
+  mutate(across(c("Sample", "Bear"), as.factor))
 
 df <- df %>%
   mutate(Season = case_when(
-    doy >= 152 & doy < 196  ~ "Early_summer",   #Samples from 1st June - 15th July
-    doy >= 197 & doy < 243 ~ "Late_summer", #Samples from 16th of July-1st of sep
-    doy >= 244 & doy <= 284 ~ "Early_autumn", #Samples from 1st of sep-11th of October
+    doy >= 137 & doy < 237  ~ "Early season",   #Samples from 1st June - 31st July
+#    doy >= 197 & doy < 243 ~ "Late_summer", #Samples from 16th of July-1st of sep
+    doy >= 237 & doy <= 284 ~ "Late season", #Samples from 1st of sep-11th of October
     TRUE ~ NA_character_
   ))
 
 
-##Create wide-format df
-
-df_wide <- df %>% 
-  select(Sample, North, doy, Season, dung_age, 
-         Species, Count) %>% 
-  pivot_wider(
-    names_from = Species,
-    values_from = Count,
-    values_fn = sum
-  )
-
 #remove rows with no species
-df_wide <- df_wide %>%
-  mutate(across(5:110, ~ replace_na(.x, 0)))
-df_wide$rowsum <- rowSums(df_wide[,5:109])
-df_wide <- df_wide[df_wide$rowsum != 0, ]
+df <- df %>%
+  mutate(across(26:129, ~ replace_na(.x, 0)))
+df$rowsum <- rowSums(df[,26:129])
+df <- df[df$rowsum != 0, ]
 
-####permanova
-df_clean <- df_wide %>%
-  select(-rowsum) %>% 
-  filter(!is.na(dung_age),
+df$North <- as.numeric(df$North)
+df$Season <- as.factor(df$Season)
+df$doy <- as.numeric(df$doy)
+
+df_clean <- df %>%
+  dplyr::select(-rowsum) %>% 
+  filter(!is.na(days_old_avg),
          !is.na(North),
          !is.na(doy),
          !is.na(Season))
 
-meta_df <- df_clean[,1:5]
-species_df <- df_clean[,6:110] 
 
-##Permanova
-dist_matrix <- vegdist(species_df, method = "jaccard", binary = TRUE)
+meta_df <- df_clean[,c(1:25,130:136)]
+species_df <- df_clean[,26:129] 
 
-Permanova_mod <- adonis2(species_df ~ dung_age + North * Season, 
+
+##RDA
+
+x <- decostand(species_df, "hellinger")
+rda_mod <- rda(x ~ days_old_avg * Season + North * Season, data = df_clean)
+
+#test significance
+anova(rda_mod, by = "margin")
+
+new_early <- subset(df_clean, Season == "Early season")
+new_late  <- subset(df_clean, Season == "Late season")
+
+early_scores <- predict(rda_mod, newdata = new_early, type = "wa")
+late_scores  <- predict(rda_mod, newdata = new_late, type = "wa")
+
+get_arrow <- function(rda_mod, term, season = NULL) {
+  
+  cf <- coef(rda_mod)
+  
+  base <- cf[term, , drop = FALSE]
+  
+  if (!is.null(season)) {
+    inter_name <- paste0("Season", season, ":", term)
+    
+    if (inter_name %in% rownames(cf)) {
+      base <- base + cf[inter_name, , drop = FALSE]
+    }
+  }
+  
+  as.numeric(base)
+}
+
+seasons <- c("Early season", "Late season")
+
+arrows <- do.call(rbind, lapply(seasons, function(s) {
+  
+  data.frame(
+    Season = s,
+    North_RDA1 = get_arrow(rda_mod, "North", s)[1],
+    North_RDA2 = get_arrow(rda_mod, "North", s)[2],
+    Dung_RDA1  = get_arrow(rda_mod, "days_old_avg", s)[1],
+    Dung_RDA2  = get_arrow(rda_mod, "days_old_avg", s)[2]
+  )
+}))
+
+
+site_scores <- as.data.frame(scores(rda_mod, display = "sites"))
+site_scores$Season <- df_clean$Season
+
+ggplot(site_scores, aes(RDA1, RDA2)) +
+  
+  # sites
+  geom_point(aes(color = Season), size = 3, alpha = 0.8) +
+  
+  # North arrows per season
+  geom_segment(data = arrows,
+               aes(x = 0, y = 0,
+                   xend = North_RDA1,
+                   yend = North_RDA2,
+                   linetype = Season),
+               color = "blue", linewidth = 1) +
+  
+  # Dung age arrows per season
+  geom_segment(data = arrows,
+               aes(x = 0, y = 0,
+                   xend = Dung_RDA1,
+                   yend = Dung_RDA2,
+                   linetype = Season),
+               color = "darkgreen", linewidth = 1) +
+  
+  theme_minimal() +
+  theme(panel.grid = element_blank()) +
+  labs(title = "Season-specific environmental effects (from interaction RDA)") 
+
+
+
+##No interactions model
+x <- decostand(species_df, "hellinger")
+rda_mod_base <- rda(x ~ days_old_avg + Season + North, data = df_clean)
+
+#test significance
+anova(rda_mod_base, by = "margin")
+
+#plot
+site_scores <- as.data.frame(scores(rda_mod_base, display = "sites"))
+species_scores <- as.data.frame(scores(rda_mod_base, display = "species"))
+env_scores <- as.data.frame(scores(rda_mod_base, display = "bp"))  # environmental arrows
+env_scores <- env_scores[!rownames(env_scores) %in% grep("^Season", rownames(env_scores), value = TRUE), ]
+row.names(env_scores) <- c("Dung age", "Latitude")
+
+site_scores$Season <- df_clean$Season
+site_scores$North <- df_clean$North
+site_scores$days_old_avg <- df_clean$days_old_avg
+
+#Plot only most influencal species
+species_scores$species <- rownames(species_scores)
+
+# pick most extreme species (longest vectors)
+species_scores$mag <- sqrt(species_scores$RDA1^2 + species_scores$RDA2^2)
+
+top_species <- species_scores[order(-species_scores$mag), ][1:10, ]
+
+rda_plot <- ggplot(site_scores, aes(RDA1, RDA2)) +
+  # sites
+  geom_point(aes(color = Season), size = 3, alpha = 0.6) +
+  scale_color_manual(values = c("#0072B2", "#009E73")) +
+  # species (ONLY top ones)
+  geom_segment(data = top_species,
+               aes(x = 0, y = 0, xend = RDA1, yend = RDA2),
+               color = "grey60", alpha = 0.6) +
+  geom_segment(data = env_scores,
+               aes(x = 0, y = 0, xend = RDA1, yend = RDA2),
+               color = "#CC79A7", linewidth = 1) +
+  geom_text_repel(data = top_species,
+                  aes(label = species),
+                  size = 3,
+                  max.overlaps = Inf) +
+  geom_text(data = env_scores,
+                  aes(label = rownames(env_scores)),
+                  color = "black", size = 4) +
+  theme_bw() +
+  theme(legend.position="right",
+                legend.text=element_text(size=12),
+                legend.title=element_text(size=14),
+                legend.direction='vertical',
+                axis.title.x = element_text(size = 14),
+                axis.title.y = element_text(size = 14),
+                axis.text = element_text(size = 12),
+                panel.grid.minor = element_blank(), 
+                panel.grid.major = element_blank())
+
+
+ggsave(rda_plot, filename = "RDA_bearfeces.png", dpi = 300, height = 5.26,
+       width = 7.5)
+
+#Calculate species richness and abundance per sample
+df_long <-  df_clean %>% 
+  pivot_longer(
+    cols = -c(1:25,130:136),
+    names_to = "Species",
+    values_to = "Count"
+  ) %>% 
+  filter(!Count %in% "0")
+
+df_richness <- df_long %>%
+  group_by(Sample, days_old_avg, North, Season) %>%
+  summarize(
+    sp_richness = n_distinct(Species),
+    abundance = sum(Count)
+  )
+
+#Summary graphs
+sum(df_richness$abundance)
+n_distinct(df_long$Species)
+nrow(df_richness)
+range(df_richness$abundance)
+range(df_richness$sp_richness)
+
+ggplot(df_richness, aes(x = abundance, y = sp_richness)) +
+  geom_point()
+
+family_lookup <- tibble(
+  Genus = c("Geotrupes", "Sciodrepoides", "Catops", "Nargus", "Aphodius", "Sphaeridium",
+            "Megasternum",  "Aacaena", 
+            "Cercyon", "Cryptopleurum", "Pteryx", "Ptiliolum", "Baeocrara", "Acrotrichis", "Atheta", "Acrotona", "Aleochara",
+            "Omalium", "Anopleta", "Anotylus", "Anthobium", "Anthophagus", "Autalia",
+            "Bisnius", "Deliphrum", "Euaesthetus", "Gabrius", "Gyrohypnus",
+            "Liogluta", "Megarthrus", "Oxypoda", "Oxytelus", "Pachyatheta",
+            "Philonthus", "Platystethus", "Proteinus", "Scaphisoma",
+            "Tachinus", "Xylodromus"
+            ),
+  Family = c("Geotrupidae", "Leiodidae", "Leiodidae", "Leiodidae", "Scarabaeidae", "Hydrophilidae",
+             "Hydrophilidae", "Hydrophilidae", "Hydrophilidae", "Hydrophilidae",
+             "Ptilidae", "Ptilidae", "Ptilidae",
+             "Ptilidae", "Staphylinidae", "Staphylinidae", "Staphylinidae",
+             "Staphylinidae", "Staphylinidae",  "Staphylinidae", "Staphylinidae", "Staphylinidae",
+             "Staphylinidae", "Staphylinidae",  "Staphylinidae", "Staphylinidae", "Staphylinidae",
+             "Staphylinidae", "Staphylinidae",  "Staphylinidae", "Staphylinidae", "Staphylinidae",
+             "Staphylinidae", "Staphylinidae",  "Staphylinidae", "Staphylinidae", "Staphylinidae",
+             "Staphylinidae", "Staphylinidae")
+)
+
+df_long <- df_long %>%
+  mutate(Genus = word(Species, 1, sep = "\\.")) %>%
+  left_join(family_lookup, by = "Genus")
+
+
+
+#GLM
+
+glm_rich <- glm.nb(
+  sp_richness ~  Season + North + days_old_avg,
+  data = df_richness)
+
+Anova(glm_rich, type = c('3'))
+
+deviance(glm_rich) / df.residual(glm_rich) 
+sim_res <- simulateResiduals(glm_rich)
+plot(sim_res)
+testZeroInflation(sim_res)
+#Modle is great!
+
+#predict
+pred <- ggpredict(glm_rich, terms = c("Season"))
+pred <- as.data.frame(ggpredict(glm_rich, terms = "Season"))
+
+richness_plot <- ggplot(df_richness, aes(x = Season, y = sp_richness)) +
+  geom_jitter(width = 0.15, alpha = 0.5, size = 3, aes(color = Season)) +
+  geom_point(data = pred, aes(y = predicted, x = x), color = "black", size = 6,
+             inherit.aes = FALSE) +
+  labs(y = "Species richness") +
+  theme_bw() +
+  scale_color_manual(values = c("#0072B2", "#009E73")) +
+  geom_errorbar(
+    data = pred,
+    aes(ymin = conf.low, ymax = conf.high, x = x),
+    width = 0.2,
+    linewidth = 1,
+    inherit.aes = FALSE
+  ) +
+  theme(legend.position="none",
+        legend.text=element_text(size=12),
+        legend.title=element_text(size=14),
+        legend.direction='vertical',
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 14),
+        axis.text = element_text(size = 12),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank())
+  
+
+ggsave(richness_plot, filename ="richness_plot.png", dpi =300,
+       height = 5.26, width = 6.5)
+
+glm_abu <- glm.nb(
+  abundance ~  North * Season  + days_old_avg,
+  data = df_richness)
+
+Anova(glm_abu, type = c('3'))
+
+deviance(glm_abu) / df.residual(glm_abu) 
+sim_res <- simulateResiduals(glm_abu)
+plot(sim_res)
+testZeroInflation(sim_res)
+#Model is decent. Nothing influences abundances
+
+
+##Permanova (add random effect of year)
+dist_matrix <- vegdist(species_df, method = "bray")
+
+Permanova_mod <- adonis2(species_df ~ days_old_avg * Season + Season * North, 
                          data=df_clean,
                          permutations=999,
-                         method = "jaccard",
-                         binary = TRUE)
+                         method = "bray",
+                         #         binary = TRUE,
+                         by = "margin")
 
-#Check assumption of homogeneity of multivariate dispersion
-anova(betadisper(dist_matrix, df_clean$dung_age))
 
 Permanova_mod
 
-NMDS_mod <- metaMDS(species_df, distance = "jaccard", k = 2, trymax = 1000)
+#Check assumption of homogeneity of multivariate dispersion
+anova(betadisper(dist_matrix, df_clean$days_old_avg))
+
+NMDS_mod <- metaMDS(species_df, distance = "bray", k = 2, trymax = 5000)
 # Run twice to avoid local optimum.
-NMDS_mod <- metaMDS(species_df, distance = "jaccard", k = 2, trymax = 1000, 
+NMDS_mod <- metaMDS(species_df, distance = "bray", k = 2, trymax = 1000, 
                     previous.best = NMDS_mod)
 
 #extract the site scores
@@ -249,28 +320,44 @@ datascores = as.data.frame(scores(NMDS_mod)$sites)
 datascores$doy = df_clean$doy
 datascores$Season = df_clean$Season
 datascores$North = df_clean$North
-datascores$dung_age = df_clean$dung_age
+datascores$days_old_avg = df_clean$days_old_avg
 
 #Add environmental variables
 env <- envfit(NMDS_mod,
-              df_clean[, c("Season", "North")],
+              df_clean[, c("North", "days_old_avg")],
               permutations = 999)
 
 ef <- as.data.frame(env$vectors$arrows * sqrt(env$vectors$r))
 ef$variable <- rownames(ef)
 
+datascores <- datascores %>%
+  mutate(Season = fct_recode(Season,
+                             "Late summer" = "Late_summer",
+                             "Early summer" = "Early_summer",
+                             "Early autumn" = "Early_autumn"
+  ))
+
+
+datascores <- datascores %>%
+  mutate(Season = fct_relevel(Season,
+                              "Early summer", "Late summer", "Early autumn"
+  ))
+
 nmds_plot <- ggplot(datascores, aes(NMDS1, NMDS2, color = Season)) +
-  geom_point() +
+  geom_point(size = 3) +
   geom_segment(data = ef,
                aes(x = 0, y = 0,
                    xend = NMDS1,
                    yend = NMDS2),
                inherit.aes = FALSE,
-               arrow = arrow(length = unit(0.2, "cm"))) +
+               arrow = arrow(length = unit(0.4, "cm")),
+               linewidth = 1.5) +
   geom_text(data = ef,
             aes(x = NMDS1, y = NMDS2, label = variable),
             inherit.aes = FALSE,
-            vjust = -0.5) +
+            vjust = -0.5,
+            size = 6) +
+  scale_color_manual(values = c("#0072B2", "#CC79A7", "#D55E00")) +
   theme_bw() +
   theme(legend.position="right",
         legend.text=element_text(size=20),
@@ -286,6 +373,10 @@ nmds_plot <- ggplot(datascores, aes(NMDS1, NMDS2, color = Season)) +
            label = paste("Stress =", round(NMDS_mod$stress, 3)), 
            hjust = 0.8, vjust = 0.5, size = 6)
 
+nmds_plot
+
 ggsave(nmds_plot, filename = "nmds_plot.png", dpi = 300,
        height = 5.26, width = 6.5)
 
+ggplot(df_clean, aes(x = dung_age_avg, y = North, color = Season)) +
+  geom_point()
